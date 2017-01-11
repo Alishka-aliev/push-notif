d9619e972a7011aa2d545db230dd41b90bda3131
Ali A <a.aliev.01@gmail.com>
2017-01-11 15:07:38 +0300

    merge fetch

---
+++ model/Db.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aliev
+ * Date: 11.01.17
+ * Time: 10:34
+ */
+
+namespace App;
+
+
+class Db
+{
+    public function __construct()
+    {
+        var_dump(111);
+    }
+}
